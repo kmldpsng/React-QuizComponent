9015e8daf7f4d4980918786ee7a43887bbb4b1d9
Kamal Deep Singh <kamal311991@gmail.com>
2019-06-09 01:45:24 +0530

    Error Checking.

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,9 +3,20 @@ import QuizQuestionButton from './QuizQuestionButton'
 
 class QuizQuestion extends Component
 {
+    constructor(props){
+        super(props);
+        this.state = {incorrectAnswer : false}
+    }
+
+
     handleClick(buttonText){
         if(buttonText === this.props.quiz_question.answer)
-        {this.props.showNextQuestionHandler()}
+        {
+            this.setState({incorrectAnswer : false});
+            this.props.showNextQuestionHandler();
+        }
+        else
+        this.setState({incorrectAnswer:true});
 
     }
     render(){
@@ -20,9 +31,9 @@ class QuizQuestion extends Component
               {this.props.quiz_question.answer_options.map((answer_options,index)=>{
                   return <QuizQuestionButton key={index} button_text ={answer_options} 
                   clickHandler ={this.handleClick.bind(this)}/>})}
-            
           </ul>
         </section>
+        {this.state.incorrectAnswer ?<p className='error'>Sorry, that's not right.</p>:null}
       </main>
       )
     }
